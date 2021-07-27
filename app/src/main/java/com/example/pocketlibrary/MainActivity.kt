c8a1cf20c313451876fd36a01df7b675fc579a4f
SadLoneWolf <djordjeandjic99@gmail.com>
2021-07-27 23:17:46 +0200

    Understood writing. Now Reading.

--- app/src/main/java/com/example/pocketlibrary/MainActivity.kt
+++ app/src/main/java/com/example/pocketlibrary/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.pocketlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 
@@ -13,6 +15,12 @@ class MainActivity : AppCompatActivity() {
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("Task")
 
-        myRef.setValue("Do the laundry!")
+        var text = findViewById<EditText>(R.id.editTextTextPersonName)
+        var actionButton = findViewById<Button>(R.id.button2)
+       actionButton.setOnClickListener{
+
+           var input = text.text.toString() // toString() because then it returns the string of text, else returns EditTextView object
+           myRef.setValue(input)
+       }
     }
 }
