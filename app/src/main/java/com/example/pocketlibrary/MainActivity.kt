b7c2031c0590609c8baed5fdf68a6197c7f0648d
SadLoneWolf <djordjeandjic99@gmail.com>
2021-07-28 11:50:52 +0200

    Learning to retirieve data

--- app/src/main/java/com/example/pocketlibrary/MainActivity.kt
+++ app/src/main/java/com/example/pocketlibrary/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 
@@ -17,10 +18,18 @@ class MainActivity : AppCompatActivity() {
 
         var text = findViewById<EditText>(R.id.editTextTextPersonName)
         var actionButton = findViewById<Button>(R.id.button2)
+        var delButton = findViewById<Button>(R.id.button3)
+
+        text.setOnClickListener{
+            delButton.visibility = Button.VISIBLE
+        }
        actionButton.setOnClickListener{
 
            var input = text.text.toString() // toString() because then it returns the string of text, else returns EditTextView object
            myRef.setValue(input)
        }
+        delButton.setOnClickListener{
+            Toast.makeText(this, "Deleting action",Toast.LENGTH_LONG).show()
+        }
     }
 }
