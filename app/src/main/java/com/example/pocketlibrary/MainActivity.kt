c6caecf6f9c9a1625a02184f43faa6e290c7a81a
SadLoneWolf <djordjeandjic99@gmail.com>
2021-07-28 15:57:30 +0200

    Done reading from Firebase

--- app/src/main/java/com/example/pocketlibrary/MainActivity.kt
+++ app/src/main/java/com/example/pocketlibrary/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -19,17 +23,31 @@ class MainActivity : AppCompatActivity() {
         var text = findViewById<EditText>(R.id.editTextTextPersonName)
         var actionButton = findViewById<Button>(R.id.button2)
         var delButton = findViewById<Button>(R.id.button3)
+        var checkText = findViewById<TextView>(R.id.textView)
 
-        text.setOnClickListener{
-            delButton.visibility = Button.VISIBLE
+        text.setOnClickListener {
+            if (delButton.visibility == Button.INVISIBLE) {
+                delButton.visibility = Button.VISIBLE
+            } else {
+                delButton.visibility = Button.INVISIBLE
+            }
         }
-       actionButton.setOnClickListener{
+        actionButton.setOnClickListener {
 
-           var input = text.text.toString() // toString() because then it returns the string of text, else returns EditTextView object
-           myRef.setValue(input)
-       }
-        delButton.setOnClickListener{
-            Toast.makeText(this, "Deleting action",Toast.LENGTH_LONG).show()
+            var input = text.text.toString() // toString() because then it returns the string of text, else returns EditTextView object
+            myRef.setValue(input)
+        }
+        delButton.setOnClickListener {
+            Toast.makeText(this, "Deleting action", Toast.LENGTH_LONG).show()
+            myRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                   checkText.text = snapshot.getValue().toString()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
         }
     }
 }
